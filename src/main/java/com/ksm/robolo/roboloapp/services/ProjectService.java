239b2308adea11602b730a0bd0c87708ebf74f55
piotrmajcher94 <piotr.majcher94@gmail.com>
2017-10-14 22:04:07 +0200

    Changed travis.yml

--- src/main/java/com/ksm/robolo/roboloapp/services/ProjectService.java
+++ src/main/java/com/ksm/robolo/roboloapp/services/ProjectService.java
@@ -2,9 +2,6 @@ package com.ksm.robolo.roboloapp.services;
 
 import com.ksm.robolo.roboloapp.tos.ProjectStubTO;
 import com.ksm.robolo.roboloapp.tos.ProjectTO;
-import com.ksm.robolo.roboloapp.tos.TaskTO;
-import com.ksm.robolo.roboloapp.tos.WorkerTO;
-import com.sun.jmx.snmp.tasks.Task;
 
 import java.util.List;
 
