a4208ebdaf1d781f5026b5fa1f51fb574ca9f5ac
Piotr Majcher <piotr.majcher94@gmail.com>
2017-10-19 20:42:04 +0200

    Update ProjectController.java

--- src/main/java/com/ksm/robolo/roboloapp/rest/impl/ProjectController.java
+++ src/main/java/com/ksm/robolo/roboloapp/rest/impl/ProjectController.java
@@ -22,7 +22,16 @@ public class ProjectController {
     public ProjectController(ProjectService projectService) {
         this.projectService = projectService;
     }
-
+    
+    //TODO @ResponseBody annotation is not necessary when class has the @RestController. Return ResponseEntities, should be:
+    /*
+    @GetMapping(path="/all")
+    @CrossOrigin
+    public ResponseEntity<Iterable<ProjectTO>> getAllProjects() {
+        Iterable<ProjectTO> projectTOS = projectService.getAllProjects();
+        return projectTOS == null ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : new ResponseEntity<>(projectTOS, HttpStatus.OK);
+    }
+    */
     @GetMapping(path = "/all")
     @CrossOrigin
     public @ResponseBody
@@ -32,6 +41,7 @@ public class ProjectController {
 
     @GetMapping(path = "/stubs/all")
     @CrossOrigin
+    //TODO @ResponseBody annotation is not necessary when class has the @RestController. Return ResponseEntities
     public @ResponseBody
     Iterable<ProjectStubTO> getAllProjectStubs() {
         return projectService.getAllProjectsStubs();
@@ -39,6 +49,7 @@ public class ProjectController {
 
     @GetMapping(path = "/{projectId}")
     @CrossOrigin
+    //TODO @ResponseBody annotation is not necessary when class has the @RestController. Return ResponseEntities
     public @ResponseBody
     ProjectTO getProject(@PathVariable String projectId) {
         ProjectTO projectTO = null;
@@ -46,6 +57,8 @@ public class ProjectController {
             Long projectIdLong = Long.valueOf(projectId);
             projectTO = projectService.getProject(projectIdLong);
             if (projectTO == null) {
+                //TODO should return HttpStatus.NOT_FOUND
+            // example: https://stackoverflow.com/questions/22536059/how-to-return-not-found-status-from-spring-controller
                 logger.info("Could not find the ProjectTO object with id: " + projectIdLong);
             }
         } catch (NumberFormatException e) {
@@ -58,6 +71,7 @@ public class ProjectController {
 
     @GetMapping(path = "/byclient/{clientId}")
     @CrossOrigin
+    //TODO @ResponseBody annotation is not necessary when class has the @RestController. Return ResponseEntities
     public @ResponseBody
     Iterable<ProjectStubTO> getAllProjectsStubForClientId(@PathVariable String clientId) {
         List<ProjectStubTO> fromClientList = null;
@@ -66,9 +80,13 @@ public class ProjectController {
             Long clientIdLong = Long.valueOf(clientId);
             fromClientList = projectService.getAllProjectStubsFromClient(clientIdLong);
             if (fromClientList == null) {
+                //TODO should return HttpStatus.NOT_FOUND
+            // example: https://stackoverflow.com/questions/22536059/how-to-return-not-found-status-from-spring-controller
                 logger.info("Could not find the ProjectTO object with id: " + clientIdLong);
             }
         } catch (NumberFormatException e) {
+            //TODO should return HttpStatus.BAD_REQUEST
+            // example: https://stackoverflow.com/questions/1364527/http-status-code-for-bad-data
             logger.error("Incorrect path variable: " + clientId);
             e.printStackTrace();
         }
