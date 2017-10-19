2f0f75c1b2d7e74428d8851a2c769d2c8260e274
Piotr Majcher <piotr.majcher94@gmail.com>
2017-10-19 20:57:24 +0200

    Update Service.java

--- src/main/java/com/ksm/robolo/roboloapp/services/Service.java
+++ src/main/java/com/ksm/robolo/roboloapp/services/Service.java
@@ -1,6 +1,7 @@
 package com.ksm.robolo.roboloapp.services;
 
-
+// TODO remove it - what's the purpose? We will implement CrudOperations interface directly. Maybe rename it to
+// CrudOperationsService so we maintain the convention
 interface Service<T> extends CrudOperations<T> {
 
 
