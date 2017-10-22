574c65a813aca89081b10b18afc6a97e79f6ff87
piotrmajcher94 <piotr.majcher94@gmail.com>
2017-10-22 16:07:11 +0200

    Merge branch 'master' into #18_Logging_in_and_user_session
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/ksm/robolo/roboloapp/rest/ProjectController.java
    #	src/main/java/com/ksm/robolo/roboloapp/tos/UserTO.java

--- src/main/java/com/ksm/robolo/roboloapp/rest/WorkerController.java
+++ src/main/java/com/ksm/robolo/roboloapp/rest/WorkerController.java
@@ -1,9 +1,10 @@
-package com.ksm.robolo.roboloapp.rest.impl;
+package com.ksm.robolo.roboloapp.rest;
 
-import com.ksm.robolo.roboloapp.domain.WorkerEntity;
 import com.ksm.robolo.roboloapp.services.impl.WorkerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
