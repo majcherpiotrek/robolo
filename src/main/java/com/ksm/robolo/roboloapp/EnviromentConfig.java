27e4be11fabb8b691c415c2d321728d17e9d3bbd
Piotr Majcher <piotr.majcher94@gmail.com>
2017-12-05 01:28:23 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/main/java/com/ksm/robolo/roboloapp/EnviromentConfig.java
+++ src/main/java/com/ksm/robolo/roboloapp/EnviromentConfig.java
@@ -1,10 +1,12 @@
 package com.ksm.robolo.roboloapp;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+@Configuration
 public class EnviromentConfig {
 
 	private static boolean developmentModeEnabled = false;
